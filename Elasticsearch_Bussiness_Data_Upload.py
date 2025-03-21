e6674fc7160c7ff2d8839d34974f504b5a204a1c
Shreemoy Nanda <nanda.shr@northeastern.edu>
2025-03-21 13:46:40 -0400

    Update Elasticsearch_Bussiness_Data_Upload.py

--- Elasticsearch_Bussiness_Data_Upload.py
+++ Elasticsearch_Bussiness_Data_Upload.py
@@ -1,21 +1,25 @@
 from elasticsearch import Elasticsearch, helpers
 import json
 
-# connect to elasticsearch cloud
+# Establish connection to Elasticsearch cluster
 client = Elasticsearch(
-    "<elasticsearch_cluster_url>",
+    "your-elasticsearch-cluster-url",  # Replace with your Elasticsearch cluster URL
     verify_certs=False,
-    basic_auth=("<user_name>", "<password>")
+    basic_auth=("your-username", "your-password")  # Replace with your Elasticsearch username and password
 )
 
-# Open the file and read json object
-with open("/kaggle/input/yelp-dataset/yelp_academic_dataset_business.json") as f:
-    data = []
+# Path to your JSON data file
+json_file_path = "/path/to/yelp_academic_dataset_business.json"  # Replace with your JSON file path
+
+# Read and parse JSON objects from file
+data = []
+with open(json_file_path) as f:
     for line in f:
         try:
             doc = json.loads(line.strip())
             data.append(doc)
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
-            
-    helpers.bulk(client, data, index="business")
+
+# Bulk upload JSON data to Elasticsearch index
+helpers.bulk(client, data, index="business")
